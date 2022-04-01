e3c168a9d7da600f57703631871c358af7d68e0c
LAPTOP-PD6QV9AN\mayo1 <mayo10002@naver.com>
2022-04-01 17:30:42 +0900

    프로필 BO 중

---
+++ src/main/java/com/sns/user/bo/ProfileBO.java
@@ -0,0 +1,21 @@
+package com.sns.user.bo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sns.user.model.ProfileView;
+
+@Service
+public class ProfileBO {
+	public List<ProfileView> generateProfileViewList(Integer userId){
+		List<ProfileView> ProfileViewList = new ArrayList<>();
+		// 프로필 사용자 목록
+		// 글 목록
+		// 팔로우 팔로워 목록
+		// 좋아요 목록 <가져올 수 있나? 
+	
+		
+	}
+}
